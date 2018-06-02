4c9df0b450d0c248abd97bab5aae2665bd3165ac
msakow1 <msakow@gmail.com>
2018-06-02 15:25:03 +0200

    Environment setup
    Initial python script for selenium use validation

---
+++ python-example.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    print(wd.capabilities)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://www.google.com/")
